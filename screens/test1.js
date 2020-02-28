89c9d5a7f2ee385032774edad84c14ed2af0897d
ethereousnatsudragneel <mnjt.singh2000@gmail.com>
2020-02-28 10:41:06 +0530

    help with PA#1

--- screens/test1.js
+++
@@ -1,6 +0,0 @@
-import React, {Component} from 'react'
-import {View,Text} from 'react-native'
-export default class FriendScreen extends Component{
-render(){return(<View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-<Text style={{fontSize:70}}>Fire</Text>
-</View>)}}
